9c5f2557bab384ef689a6d53e5cab6dd63ffac56
Allanf-Rodrigues <allanfcesar90@gmail.com>
2021-07-27 10:00:23 -0300

    Corrigindo kmtotal

--- app/Http/Controllers/ExerciciosController.php
+++ app/Http/Controllers/ExerciciosController.php
@@ -5,29 +5,45 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Exercicio;
+use App\User;
+
+
+use Illuminate\Support\Facades\DB;
+
 use App\Http\Requests\ExercicioRequest;
 
 class ExerciciosController extends Controller
 {
-   
+
     public function index()
     {
+
         $exercicio = Exercicio::all();
-        return view('exercicio.index', compact('exercicio'));
+        $kmtotal = DB::table('exercicios')->sum('kmatual');
+
+        return view('exercicio.index', compact('exercicio' , 'kmtotal'));
     }
 
     public function create()
     {
+        $usuario = User::all();
         $exercicio = Exercicio::all();
-        return view('exercicio.create', compact('exercicio'));
+        return view('exercicio.create', compact('exercicio','usuario'));
+
     }
 
     public function store(ExercicioRequest $request)
     {
+
         $exercicio = new Exercicio();
+
+        //$users = DB::table('users')->get();
+
+
         $exercicio->periodo = $request->input('periodo');
         $exercicio->kmatual = $request->input('kmatual');
         $exercicio->kmtotal = $request->input('kmtotal');
+        $exercicio->user_id = $request->input('user_id');
         $exercicio->tempo = $request->input('tempo');
         $exercicio->save();
         return redirect()->route('exercicio.index', compact('exercicio'));
@@ -35,10 +51,10 @@ class ExerciciosController extends Controller
 
     public function show($id)
     {
-        
+
     }
 
-    
+
     public function edit($id)
     {
         $exercicio = Exercicio::find($id);
@@ -62,7 +78,7 @@ class ExerciciosController extends Controller
         return redirect()->route('exercicio.index', compact('exercicio'));
     }
 
-    
+
     public function destroy($id)
     {
         $exercicio = Exercicio::find($id);
--- resources/views/exercicio/create.blade.php
+++ resources/views/exercicio/create.blade.php
@@ -1,19 +1,20 @@
 @extends('layouts.app')
 @section('content')
-<div class="container"> 
-
+<div class="container">
 {{-- @if($errors->any())
             <div class="alert">
                 @foreach ($errors->all() as $message)
                     <p>{{ $message}}</p>
                 @endforeach
-            </div> 
+            </div>
         @endif --}}
 
         <form id="frmexercicio" name="frmexercicio" action="{{ route('exercicio.store') }}" method="POST">
         @csrf
+
+         
         <div class="form-group col-6">
-          <label for="periodo">Período:</label>
+          <label for="periodo">Periodo:</label>
           <input type="text" class="form-control {{ $errors->has('periodo') ? 'is-invalid' : '' }}" id="periodo" name="periodo" placeholder="Informe o período">
             @if ($errors->has('nome'))
             <div class="invalid-feedback">
@@ -32,7 +33,7 @@
             @endif
         </div>
 
-        <div class="form-group col-6">
+        <!-- <div class="form-group col-6">
           <label for="kmtotal">Km total:</label>
           <input type="text" class="form-control {{ $errors->has('kmtotal') ? 'is-invalid' : '' }}" id="kmtotal" name="kmtotal" placeholder="Informe a quilometragem total">
             @if ($errors->has('nome'))
@@ -40,7 +41,8 @@
                 {{$errors->first('kmtotal', 'Atenção esse campo é obrigatório')}}
             </div>
             @endif
-        </div>
+        </div> -->
+
 
         <div class="form-group col-6">
           <label for="tempo">Tempo:</label>
@@ -51,6 +53,24 @@
             </div>
             @endif
         </div>
+
+        {{-- <div class="col-4">
+            <div class="form-group">
+                <select class="form-control" id="usuario" name="usuario">
+                <option>Selecione</option>
+                @foreach($usuario as $usuarios)
+                <option value="{{$usuarios->id}}">{{$usuarios->name}}</option>
+                @endforeach
+                </select>
+            </div>
+          </div> --}}
+
+
+
+          <div class="form-group col-6">
+            <label for="user">Cod. Usuario:</label>
+            <input type="text" class="form-control" id="user_id" name="user_id" placeholder="Informe o id do usuario">
+          </div>
         <button type="submit" class="btn btn-primary">Adicionar</button>
       </form>
 </div>
